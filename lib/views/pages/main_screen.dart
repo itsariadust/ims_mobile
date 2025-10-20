21181b49472807fa9cc49c7df23d796e3581916b
itsariadust <jakobaindreas11@gmail.com>
2025-10-21 00:51:47 +0800

    UI: Made the navigation bar adhere to M3 standards

--- lib/views/pages/main_screen.dart
+++ lib/views/pages/main_screen.dart
@@ -17,10 +17,10 @@ class _MainScreenState extends ConsumerState<MainScreen> {
   int _selectedIndex = 0;
 
   final List<_NavItem> _navItems = [
-    _NavItem('Home', Icons.home, HomeScreen()),
-    _NavItem('Inventory', Icons.inventory, InventoryScreen()),
-    _NavItem('Suppliers', Icons.store, SuppliersScreen()),
-    _NavItem('Transactions', Icons.receipt, TransactionsScreen()),
+    _NavItem('Home', Icons.home_outlined, Icons.home, HomeScreen()),
+    _NavItem('Inventory', Icons.inventory_2_outlined, Icons.inventory, InventoryScreen()),
+    _NavItem('Suppliers', Icons.store_outlined, Icons.store, SuppliersScreen()),
+    _NavItem('Transactions', Icons.receipt_outlined, Icons.receipt, TransactionsScreen()),
   ];
 
   void _onItemTapped(int index) {
@@ -47,12 +47,12 @@ class _MainScreenState extends ConsumerState<MainScreen> {
       ),
       drawer: AppDrawer(),
       body: _navItems[_selectedIndex].widget,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-        items: _navItems.map((item) {
-          return BottomNavigationBarItem(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTapped,
+        destinations: _navItems.map((item) {
+          return NavigationDestination(
+            selectedIcon: Icon(item.selectedIcon),
             icon: Icon(item.icon),
             label: item.label,
           );
@@ -65,7 +65,8 @@ class _MainScreenState extends ConsumerState<MainScreen> {
 class _NavItem {
   final String label;
   final IconData icon;
+  final IconData selectedIcon;
   final Widget widget;
 
-  const _NavItem(this.label, this.icon, this.widget);
+  const _NavItem(this.label, this.icon, this.selectedIcon, this.widget);
 }
