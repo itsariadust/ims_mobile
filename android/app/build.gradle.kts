6439154cb8e1ea1466b312a0f0affa0907908d94
itsariadust <jakobaindreas11@gmail.com>
2025-10-27 02:54:34 +0800

    chore: changed package name
    (cherry picked from commit 6feb7ed8d7bf29d3f955b9c18a0be0f0b8572220)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ims_mobile"
+    namespace = "com.ariadust.ims_mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ims_mobile"
+        applicationId = "com.ariadust.ims_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 29
