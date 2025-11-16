dd576ed85bcd2fd486ae1b30639afa023b97c05e
itsariadust <jakobaindreas11@gmail.com>
2025-11-17 01:00:33 +0800

    feature: added added new method for adding and updating supplier in the service layer

--- lib/services/supplier_service.dart
+++ lib/services/supplier_service.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ims_mobile/core/network/api_client.dart';
+import 'package:ims_mobile/models/supplier/new_supplier_api_model.dart';
 import 'package:ims_mobile/models/supplier/supplier_api_model.dart';
 
 final supplierServiceProvider = Provider<SupplierService>((ref) {
@@ -32,6 +33,32 @@ class SupplierService {
       return SupplierApiModel.fromJson(response.data);
     } on DioException catch (e) {
       throw Exception(e.toString());
+    }
+  }
+
+  Future<SupplierApiModel> addSupplier(NewSupplierApiModel supplier) async {
+    try {
+      final jsonData = supplier.toJson();
+
+      final response = await _dio.post(_supplierEndpoint, data: jsonData);
+
+      return SupplierApiModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<SupplierApiModel> updateSupplier(int id, SupplierApiModel supplier) async {
+    try {
+      final jsonData = supplier.toJson();
+
+      final response = await _dio.patch('$_supplierEndpoint/$id', data: jsonData);
+
+      return SupplierApiModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception(e.toString());
+    } catch (e) {
+      throw Exception('An unknown error occurred');
     }
   }
 }
